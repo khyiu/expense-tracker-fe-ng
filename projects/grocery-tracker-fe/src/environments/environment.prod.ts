fd42d8fae7ace0873e77f2a04edc5251435b88c9
khyiu <superk888@gmail.com>
2020-12-07 23:11:25 +0100

    #2 Complete environment config for PROD mode

--- projects/grocery-tracker-fe/src/environments/environment.prod.ts
+++ projects/grocery-tracker-fe/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  awsCognitoConfig: {
+    loginEndpoint:
+      'https://grocery-tracker-back-8.auth.eu-central-1.amazoncognito.com/login',
+    loginFlowType: 'code',
+    clientId: '4uq3crrcu59cbc408a4f7acijl',
+    clientSecret: 'lo6vt6svaq6vejgk0upuockvaqnf23k755i3f10j0dpfdiioboo',
+    loginRedirectUri: 'http://localhost:4200/purchases',
+    tokenEndpoint:
+      'https://grocery-tracker-back-8.auth.eu-central-1.amazoncognito.com/oauth2/token',
+  },
 };
